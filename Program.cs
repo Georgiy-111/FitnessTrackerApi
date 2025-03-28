2619525481add3b05e777f3b31399a7bd4b3cc2c
Georgiy <Ger900005@mail.ru>
2025-03-28 16:22:10 +0700

    #1337 Удалить неиспользуемые WriteLine

--- Program.cs
+++ Program.cs
@@ -34,8 +34,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-Console.WriteLine("Hello");
-Console.WriteLine("Hello World!");
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
